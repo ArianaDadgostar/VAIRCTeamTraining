a52deff40389c9cd81088b83426a652b3420f569
Ariana Dadgostar <ariana.dadgostar@gmail.com>
2026-03-15 11:32:54 -0700

    Created new byte array.

--- VirtualProjects/SmartPointers/main.cpp
+++ VirtualProjects/SmartPointers/main.cpp
@@ -41,76 +41,10 @@ struct Node
 template<typename T>
 struct LinkedList
 {
-    int length = 8;
-    std::array<Node<T>, 8> nodes;
-    std::byte used;
-
-    LinkedList(T headVal)
-    {
-        nodes[0].value = headVal;
-        std::byte current = std::byte{1};
-        for(int i = 1; i < length; i ++)
-        {
-            nodes[i - 1].next = &nodes[i];
-            current << 1;
-        }
-        
-        used xor current;
-    }
-
-    void add(T val)
-    {
-        for(int i = 0; i < length; i++)
-        {
-            if(used[i] == true) continue;
-            nodes[i].value = val;
-            used[i] = true;
-            return;
-        }  
-    }
-
-    void remove(T val)
-    {
-        for(int i = 0; i < length; i++)
-        {
-            if(nodes[i].value != val) continue;
-
-            nodes[i].value = T();
-            nodes[i].next = nullptr;
-            used[i] = false;
-            return;
-        }
-    }
+    std::byte[] nodes;
 };
+
 
 int main() 
 {
-    Alien original(1000, "Jimmy");
-
-    std::cout<< "Copies: " << *original.copies << std::endl;
-    Alien copy1 = Alien(1000, "Jimmy");
-    copy1 = original;
-    std::cout<< "Copies: " << *original.copies << std::endl;
-    Alien copy2 = Alien(1000, "Jimmy");
-    copy2  = original;
-    std::cout<< "Copies: " << *original.copies << std::endl;
-
-    ~copy1;
-    std::cout<< "Copies: " << *original.copies << std::endl;
-    ~copy2;
-    std::cout<< "Copies: " << *original.copies << std::endl;
-
-    LinkedList<int> list(0);
-    for(int i = 1; i < list.length; i++)
-    {
-        list.add(i);
-    }
-
-    list.remove(2);
-    list.add(17);
-
-    for(Node node: list.nodes)
-    {
-        std::cout<< node.value << std::endl;
-    }
 }
