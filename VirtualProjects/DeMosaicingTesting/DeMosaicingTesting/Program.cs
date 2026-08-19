f11f8ef4b527059147e4e61ab909c00dd3dcb607
Ariana Dadgostar <ariana.dadgostar@gmail.com>
2026-08-18 19:33:56 -0700

    Got some color value, but seems offset. Messing with current traversals.

--- VirtualProjects/DeMosaicingTesting/DeMosaicingTesting/Program.cs
+++ VirtualProjects/DeMosaicingTesting/DeMosaicingTesting/Program.cs
@@ -25,7 +25,7 @@ namespace DemosaicAPI
             return true;
         }
 
-        static int Calculate_Green(int north, int east, int west, int south, int current,
+        public static int Calculate_Green(int north, int east, int west, int south, int current,
                                    int gNorth, int gEast, int gWest, int gSouth)
         {
             nChange = Math.Abs(north - current) * 2 + Math.Abs(gNorth - gSouth);
@@ -43,7 +43,7 @@ namespace DemosaicAPI
                 return (gSouth * 3 + gNorth + current - south) / 4;
         }
 
-        static int Calculate_Diagonal(int current, int NE, int SE, int NW, int SW, 
+        public static int Calculate_Diagonal(int current, int NE, int SE, int NW, int SW, 
                                       int altNE, int altSE, int altNW, int altSW,
                                       int gCurrent, int gNE, int gSE, int gNW, int gSW)
         {
@@ -69,23 +69,110 @@ namespace DemosaicAPI
         {
             //Vec3b pixel = mat.At<Vec3b>(i, j);
             int index = 0;
+            int current = 0;
+            int value = 0;
+            int altValue = 0;
 
             Vec3b[] RGBVals;
             mat.GetArray(out RGBVals);
 
-            for(int i = 1; i < mat.Rows; i += 2 )
+            // for(int i = 0; i < mat.Rows; i++)
+            // {
+            //     for(int j = 0; j < mat.Cols; j++)
+            //     {
+            //         current = i * mat.Cols + j;
+            //         if(i % 2 == 0 && j % 2 == 0) // RED PIXEL
+            //         {
+            //             RGBVals[current][1] = 0;
+            //             RGBVals[current][2] = 0;
+            //         }
+            //         else if(i % 2 == 0 && j % 2 == 1) // GREEN PIXEL
+            //         {
+            //             RGBVals[current][0] = 0;
+            //             RGBVals[current][2] = 0;
+            //         }
+            //         else if(i % 2 == 1 && j % 2 == 0) // GREEN PIXEL
+            //         {
+            //             RGBVals[current][0] = 0;
+            //             RGBVals[current][2] = 0;
+            //         }
+            //         else if(i % 2 == 1 && j % 2 == 1) // BLUE PIXEL
+            //         {
+            //             RGBVals[current][0] = 0;
+            //             RGBVals[current][1] = 0;
+            //         }
+            //     }
+            // }
+
+            for(int i = 2; i < mat.Rows - 2; i += 1 )
             {
-                for(int j = index; j < mat.Cols; j += 2) // Applies masks
+                for(int j = index; j < mat.Cols - 1; j += 2)
                 {
-                    RGBVals[i * mat.Cols + j] = (Vec3b)PixelGrouping.Hue_Transit(RGBVals[i * mat.Cols + j - 3],
-                                                                                RGBVals[i * mat.Cols + j],
-                                                                                RGBVals[i * mat.Cols + j + 3], 
-                                                                                RGBVals[i * mat.Cols + j - 2], 
-                                                                                RGBVals[i * mat.Cols + j + 4]);
+                    current = i * mat.Cols + j;
+                    value = (index == 0) ? 0 : 2;
+                    RGBVals[current][1] = (byte)PixelGrouping.Calculate_Green
+                        (RGBVals[current - 2 * mat.Cols][value], // NORTH
+                        RGBVals[current + 2][value], // EAST
+                        RGBVals[current - 2][value], // WEST
+                        RGBVals[current + 2 * mat.Cols][value], // SOUTH
+                        RGBVals[current][value],
+                        RGBVals[current - mat.Cols][1], // GREEN NORTH
+                        RGBVals[current + 1][1], // GREEN EAST
+                        RGBVals[current - 1][1], // GREEN WEST
+                        RGBVals[current + mat.Cols][1]); // GREEN SOUTH
+                }
+                index = (index == 1) ? 0 : 1;
+            }
+            
+            index = 2;
+            for(int i = 1; i < mat.Rows - 1; i += 1 )
+            {
+                for(int j = index; j < mat.Cols - 1; j += 2)
+                {
+                    current = i * mat.Cols + j;
+
+                    RGBVals[current][2] = (byte)PixelGrouping.Hue_Transit
+                        (RGBVals[current - 1][1], // GREEN --> BLUE
+                        RGBVals[current][1],
+                        RGBVals[current + 1][1], 
+                        RGBVals[current - 1][2], 
+                        RGBVals[current + 1][2]);
+                    RGBVals[current][0] = (byte)PixelGrouping.Hue_Transit
+                        (RGBVals[current - mat.Cols][1], // GREEN --> RED
+                        RGBVals[current][1],
+                        RGBVals[current + mat.Cols][1],
+                        RGBVals[current - mat.Cols][0],
+                        RGBVals[current + mat.Cols][0]);
                 }
                 index = (index == 2) ? 1 : 2;
             }
 
+            index = 2;
+            for(int i = 2; i < mat.Rows - 2; i += 1)
+            {
+                for(int j = index; j < mat.Cols - 2; j += 2)
+                {
+                    value = (index == 2) ? 0 : 2;
+                    altValue = (value == 0) ? 2 : 0;
+                    RGBVals[current][value] = (byte)PixelGrouping.Calculate_Diagonal(RGBVals[current][value],
+                        RGBVals[current - mat.Cols + 2][value], // NE
+                        RGBVals[current + mat.Cols + 2][value], // SE
+                        RGBVals[current - mat.Cols - 2][value], // NW
+                        RGBVals[current + mat.Cols - 2][value], // SW
+                        RGBVals[current - mat.Cols + 1][altValue], // altNE
+                        RGBVals[current + mat.Cols + 1][altValue], // altSE
+                        RGBVals[current - mat.Cols - 1][altValue], // altNW
+                        RGBVals[current + mat.Cols - 1][altValue], // altSW
+                        RGBVals[current][1], // GREEN CURRENT
+                        RGBVals[current - mat.Cols + 1][1], // GREEN NE
+                        RGBVals[current + mat.Cols + 1][1], // GREEN SE
+                        RGBVals[current - mat.Cols - 1][1], // GREEN NW
+                        RGBVals[current + mat.Cols - 1][1]); // GREEN SW
+                }
+                index = (index == 2) ? 1 : 2;
+            }
+
+            mat.SetArray(RGBVals);
             Cv2.ImShow("My Image", mat);
             Cv2.WaitKey(0); 
             Cv2.DestroyAllWindows();
